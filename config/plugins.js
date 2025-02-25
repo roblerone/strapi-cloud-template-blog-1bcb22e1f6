e142a86ecb16bfca64b0190c43e6dddc3ebaadc0
Rob van der Vinden <roblerone@users.noreply.github.com>
2025-02-25 11:37:48 +0100

    Add Mux plugin

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,9 @@
-module.exports = () => ({});
+module.exports = ({env}) => ({
+    "mux-video-uploader": {
+        enabled: true,
+        config: {
+            accessTokenId: env('MUX_ACCESS_TOKEN_ID'),
+            secretKey: env('MUX_ACCESS_TOKEN_SECRET')
+        }
+    }
+});
